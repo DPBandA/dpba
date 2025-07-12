ade0c37f7145d1e515d76fa4d5b04908160319b0
Desmond Bennett <desbenn@yahoo.com>
2025-07-12 11:02:36 -0500

    General updates.

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,13 @@ const CACHE = 'dpb-cache-v1';
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
-    '/assets/css/main.css',
+    '/assets/css/font-awesome.min.css',
+    '/assets/css/global.css',
+    '/assets/css/responsive.css',
+    '/assets/css/style.css',
+    '/assets/css/utility.css',
     '/assets/js/main.js',
+    '/assets/js/lunr.js',
     '/assets/manifest.json',
     '/assets/icons/icon-192.png',
     '/assets/icons/icon-512.png'

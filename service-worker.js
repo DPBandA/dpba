6dd02ef61a576b886dec0fc6a16214c5b98d1874
Desmond Bennett <desbenn@yahoo.com>
2025-07-12 10:50:01 -0500

    General updates.

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = 'dpb-cache-v1';
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/assets/css/main.css',
+    '/assets/js/main.js',
+    '/assets/manifest.json',
+    '/assets/icons/icon-192.png',
+    '/assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+            caches.open(CACHE).then(cache => cache.addAll(FILES_TO_CACHE))
+            );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+            caches.match(event.request).then(response => response || fetch(event.request))
+            );
+});
